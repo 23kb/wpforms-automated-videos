5c0683e2146a22446856de309dc036940f3930ca
Umair <061.umair@gmail.com>
2026-05-07 00:36:31 +0500

    Add frame-driven Lottie scrubbing

--- videos/lottie-sandbox/chapters/scrub.js
+++ videos/lottie-sandbox/chapters/scrub.js
@@ -9,19 +9,31 @@ export async function setup() {}
 
 export default [
   {
-    id: 'bumper',
-    chapter: 'bumper',
+    id: 'scrub',
+    chapter: 'scrub',
     duration: 0.2,
     effect: async () => {
       const gsap = await loadGsap({ flip: false, motionPath: false });
       await loadLottie();
       const lottie = mountLottie('/videos/lottie-sandbox/assets/bumper.json');
       await waitForLottie(lottie.animation);
-      const duration = lottie.animation.getDuration(false);
 
       const tl = gsap.timeline();
-      lottie.tweenInto(tl, { duration, position: 0 });
-      tl.to({}, { duration: 1 });
+      lottie.tweenFrames(tl, {
+        from: 0,
+        to: 30,
+        duration: 1.0,
+        ease: 'power2.out',
+        position: 0,
+      });
+      tl.to({}, { duration: 0.5 });
+      lottie.tweenFrames(tl, {
+        from: 30,
+        to: 10,
+        duration: 0.8,
+        ease: 'power1.inOut',
+        position: '>',
+      });
       await tlDone(tl);
 
       lottie.dispose();
